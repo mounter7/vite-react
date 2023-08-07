8e28c0edc26f0cda872771bc2a38bdec9f03278d
mounter7 <teeyok336@outlook.com>
2023-08-07 18:54:07 +0530

    updated css in SearchDataList component

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -4,7 +4,7 @@ import SearchResult from './SearchResult'
 export default function SearchResultsList({ results }) {
     return (
         <div className='relative'>
-            <div className='max-h-[400px]  overflow-y-scroll my-2 w-full absolute'>
+            <div className='my-2 w-full absolute'>
             <ul>
                 {
                     results.map(({ key, name }) => {
